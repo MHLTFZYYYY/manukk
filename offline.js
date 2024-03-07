6d6444ecf89d824b5f3d6b86c83d538d47bb2e00
MHLTFZYYYY <161667967+MHLTFZYYYY@users.noreply.github.com>
2024-03-07 09:09:30 +0700

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1709777277,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/bg_-sheet0.png",
+		"images/flappybirdbackground-sheet0.png",
+		"images/flappybirdbackgroundvnzm62ypakupicll-sheet0.png",
+		"images/tiangremovebgpreview-sheet0.png",
+		"images/pngremovebgpreview-sheet0.png",
+		"images/pngremovebgpreview2-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
